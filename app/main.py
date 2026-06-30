bb8c7e5aedd1bd3a8b0a3c461fd568c5151aae6a
Richard Nguyen <11547131+nguyri@users.noreply.github.com>
2026-06-30 16:32:21 -0600

    Change main to use local flight_checker

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import io
-from app.flight_checker import run_extraction_pipeline
-from app.flight_checker import run_optimization_pipeline
-from app.pdf_output import save_pipeline_to_pdf
+from flight_checker import run_extraction_pipeline
+from flight_checker import run_optimization_pipeline
+from pdf_output import save_pipeline_to_pdf
 
 import logging
 import sys
